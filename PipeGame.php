9c7d095bb228b4b882ceff593242c2274910eea2
Kenneth.d.birge <kenneth.d.birge@wmich.edu>
2017-11-10 13:38:16 -0500

    adding PipeGame.php

---
+++ PipeGame.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="utf-8">
+	<title>Pipe Game</title>
+	<link href="Pipe.css" rel="stylesheet">
+</head>
+<body>
+<header>
+	<p>Welcome to the pipe game</p>
+</header>
+<article>
+	<table class="pipeTable">
+		<?php include 'PipeCreator.php'; ?>
+	</table>
+</article>
+<footer>
+	Instructions here?
+</footer>
+</body>
+</html>
