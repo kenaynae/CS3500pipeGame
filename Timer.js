a97cf06faea756e90790356e64f4322c72187f24
personNumber8 <33554571+personNumber8@users.noreply.github.com>
2017-11-17 15:26:43 -0500

    Add files via upload

---
+++ Timer.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function(){
+ update();  
+});
+function resetting(){
+location.reload();
+}
+
+let start=0;
+
+
+function update(end = 0){
+    time = 50000;
+    const deltaTime = end - start;
+    tl= (time-deltaTime)/1000;
+    timeLeft=tl.toFixed(1);
+    if(time-deltaTime<0){
+        resetting();
+    }
+    document.getElementById("timer").innerHTML="you have " + (timeLeft) + " secs left";
+    
+    requestAnimationFrame(update);
+}
