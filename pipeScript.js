f3926f5ffc099189a471cdd91a49531cef0805b6
Bishop <jhs3920@wmich.edu>
2017-11-17 17:04:17 -0500

    5:04

--- pipeScript.js
+++ pipeScript.js
@@ -6,7 +6,7 @@ var pipeCount = 36,
     entrance,
     exit;
 
-entrance = (Math.floor(Math.random() * exitCount)+1);
+entrance = (Math.floor(Math.random() * exitCount) + 1);
 do {
     exit = (Math.floor(Math.random() * exitCount)+1);
 } while (entrance == exit);
@@ -17,9 +17,12 @@ for (i = 0; i <= Math.sqrt(pipeCount) + 1; i++) {
     if (i == 0 || i == Math.sqrt(pipeCount) + 1) {
         pipeArray[i] = new Array();
         for (j = 0; j <= Math.sqrt(pipeCount) + 1; j++) {
-		    if ((entrance == j || exit == j) && i == 0) {
+		    if ((entrance == j || exit == j) && i == 0 
+                && j != 7) {
                 pipeArray[i][j] = "images/ExitUp.png";
-            } else if (((entrance-18) == j || (exit-18) == j) && i == Math.sqrt(pipeCount) + 1) {
+            } else if (((entrance-18) == j || (exit-18) == j) 
+                       && i == Math.sqrt(pipeCount) + 1 
+                       && j != 0) {
                 pipeArray[i][j] = "images/ExitDown.png";
             } else {
                 pipeArray[i][j] = "images/Blank.png";
@@ -28,13 +31,10 @@ for (i = 0; i <= Math.sqrt(pipeCount) + 1; i++) {
     } else {
         pipeArray[i] = new Array();
 	    for (j = 0; j <= Math.sqrt(pipeCount) + 1; j++) {
-            if (j == 0 
-                && ([7, 9, 11, 13, 15, 17].includes(entrance) 
-                    || [7, 9, 11, 13, 15, 17].includes(exit))) {
+            if (j == 0  && (((i * 2)+5) == entrance || ((i * 2)+5) == exit)) {
                 pipeArray[i][j] = "images/ExitLeft.png";
             } else if (j == Math.sqrt(pipeCount) + 1 
-                       && ([8, 10, 12, 14, 16, 18].includes(entrance) 
-                    || [8, 10, 12, 14, 16, 18].includes(exit))){
+                       && (((i * 2)+6) == entrance || ((i * 2)+6) == exit)) {
                 pipeArray[i][j] = "images/ExitRight.png";
             }else if (j == 0 || j == Math.sqrt(pipeCount) + 1) {
                 pipeArray[i][j] = "images/Blank.png";
