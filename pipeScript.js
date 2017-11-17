e1e0fadd44a6e290bbe4b36fef42050659ff24c9
Kenneth.d.birge <kenneth.d.birge@wmich.edu>
2017-11-17 14:47:02 -0500

    updated js and main html

--- pipeScript.js
+++ pipeScript.js
@@ -1,12 +1,24 @@
-var pipeArray;
+var pipeCount = 36;
+var pipeArray = new Array();
 
-pipeArray
+pipeArray[0] = new Array();
 
-$("img").click(function() {
-	if (pipeToSwap == null)
-		pipeToSwap = $(this);
-	else {
-		$(this) = pipeToSwap;
-		pipeToSwap = null;
+for (var i = 1; i <= Math.sqrt(pipeCount); i ++) {
+	pipeArray[i] = new Array();
+	for (var j = 1; j <= Math.sqrt(pipeCount); j ++) {
+		pipeArray[i][j] = "images/Pipe" + (Math.floor(Math.random() * 6) + 1) + ".png";
 	}
-});
+}
+pipeArray.push(new Array());
+
+function writePipes() {
+	for (var i = 1; i <= Math.sqrt(pipeCount); i ++) {
+		document.write("<tr>");
+		for (var j = 1; j <= Math.sqrt(pipeCount); j ++) {
+			document.write("<td><img src=");
+			document.write(pipeArray[i][j]);
+			document.write(' class="Pipe"></td>');
+		}
+		document.write("</tr>");
+	}
+}
