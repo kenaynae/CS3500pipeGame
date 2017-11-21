45217c0ff65a812f052295bfe91eb2152cb5762e
Bishop <jhs3920@wmich.edu>
2017-11-21 18:13:59 -0500

    formatting

--- pipeScript.js
+++ pipeScript.js
@@ -9,30 +9,28 @@ var pipeCount = 36,
 entrance = (Math.floor(Math.random() * exitCount) + 1);
 do {
     exit = (Math.floor(Math.random() * exitCount) + 1);
-} while (entrance == exit);
+} while (entrance === exit);
 
 for (i = 0; i <= Math.sqrt(pipeCount) + 1; i++) {
-    if (i === 0 || i == Math.sqrt(pipeCount) + 1) {
+    if (i === 0 || i === Math.sqrt(pipeCount) + 1) {
         pipeArray[i] = new Array();
         for (j = 0; j <= Math.sqrt(pipeCount) + 1; j++) {
-		    if ((entrance == j || exit == j) && i === 0 && j != 7)
-            {
+		    if ((entrance === j || exit === j) && i === 0 && j !== 7) {
                 pipeArray[i][j] = "images/ExitUp.png";
-            } else if (((entrance-18) == j || (exit-18) == j) && i == Math.sqrt(pipeCount) + 1 && j !== 0) {
+            } else if (((entrance - 18) === j || (exit - 18) === j) && i === Math.sqrt(pipeCount) + 1 && j !== 0) {
                 pipeArray[i][j] = "images/ExitDown.png";
             } else {
-               pipeArray[i][j] = "images/Blank.png";
+                pipeArray[i][j] = "images/Blank.png";
             }
         }
     } else {
         pipeArray[i] = new Array();
 	    for (j = 0; j <= Math.sqrt(pipeCount) + 1; j++) {
-            if (j === 0  && (((i * 2)+5) == entrance || ((i * 2)+5) == exit)) {
+            if (j === 0  && (((i * 2) + 5) === entrance || ((i * 2) + 5) === exit)) {
                 pipeArray[i][j] = "images/ExitLeft.png";
-            } else if (j == Math.sqrt(pipeCount) + 1  && (((i * 2)+6) == entrance || ((i * 2)+6) == exit)) {
+            } else if (j === Math.sqrt(pipeCount) + 1  && (((i * 2) + 6) === entrance || ((i * 2) + 6) === exit)) {
                 pipeArray[i][j] = "images/ExitRight.png";
-            }
-            else if (j === 0 || j == Math.sqrt(pipeCount) + 1) {
+            } else if (j === 0 || j === Math.sqrt(pipeCount) + 1) {
                 pipeArray[i][j] = "images/Blank.png";
             } else {
                 pipeArray[i][j] = "images/Pipe" + (Math.floor(Math.random() * 6) + 1) + ".png";
@@ -46,32 +44,22 @@ pipeArray.push(new Array());
 function writePipes() {
     "use strict";
 	for (i = 0; i <= Math.sqrt(pipeCount) + 1; i++) {
-		if(i===0 || i == Math.sqrt(pipeCount) + 1){
-        document.write("<tr id='edge'>");
-		    
-        }
-        else{
-        document.write("<tr>");
+		if (i === 0 || i === Math.sqrt(pipeCount) + 1) {
+            document.write("<tr id='edge'>");
+        } else {
+            document.write("<tr>");
         }
         for (j = 0; j <= Math.sqrt(pipeCount) + 1; j++) {
-<<<<<<< HEAD
-		if(j===0 || i===0 || j== Math.sqrt(pipeCount) + 1 || i== Math.sqrt(pipeCount) + 1 ){
-=======
-		if(j===0 || j == Math.sqrt(pipeCount) + 1 || i == 0 || i == Math.sqrt(pipeCount) + 1){
->>>>>>> 69a2f3790a738715d0ddd882a96c1dcb41f58f14
-        
-        	document.write("<td id='edge'><img src=");
-			document.write(pipeArray[i][j]);
-			document.write(' class="Pipe"></td>');
-		
-        }
-        else{
-        	document.write("<td><img src=");
-			document.write(pipeArray[i][j]);
-			document.write(' class="Pipe"></td>');
-		}
+		    if (j === 0 || i === 0 || j === Math.sqrt(pipeCount) + 1 || i === Math.sqrt(pipeCount) + 1) {
+                document.write("<td id='edge'><img src=");
+			    document.write(pipeArray[i][j]);
+			    document.write(' class="Pipe"></td>');
+            } else {
+        	    document.write("<td><img src=");
+			    document.write(pipeArray[i][j]);
+			    document.write(' class="Pipe"></td>');
+		    }
         }
         document.write("</tr>");
-	
-}
+    }
 }
