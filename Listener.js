8debe02559ab68864eca48a9e14ae68e9f12391c
personNumber8 <dakota.m.grant@wmich.edu>
2017-11-21 19:16:06 -0500

    7:16

--- Listener.js
+++ Listener.js
@@ -1,13 +1,19 @@
 var img1;
 var holder;
+var i=0;
 $(".Pipe").click(function() {
 	if (!(($(this).parent().attr("id")) == "edge")) {
 	if (img1 == null)
 		img1 = $(this).attr("src");
 	else {
+		i++;
 		holder = this.src;
 		this.src = img1;
 		img1 = holder;
+		//console.log(i);
+		
+		document.getElementById("Moves").innerHTML="Number of moves " +i;
+		//moves.innerHTML(i).
 	}
 	$(".Holder").attr("src", img1)
 }
